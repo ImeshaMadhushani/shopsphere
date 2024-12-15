af5193c5e56e12a825c79d4b99970b0aebd2a9f2
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-12-15 10:03:04 +0530

    create App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Sale from './pages/Sale';
+import Auth from './pages/Auth';
+import ProceedOrder from './pages/ProceedOrder';
+import Category from './pages/Category';
+import AllProducts from './pages/AllProducts'
+import { CartProvider } from './contexts/CartContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/product/:id" element={<ProductDetail/>} />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="/sale" element={<Sale/>} />
+            <Route path="/auth" element={<Auth/>} />
+            <Route path="/proceed-order" element={<ProceedOrder/>} />
+            <Route path="/category/:categoryName" element={<Category />} />
+            <Route path="/all-products" element={<AllProducts/>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+      </Router>
+    </CartProvider>
   );
 }
 
 export default App;
+
